331c9fe0cb65d003f9375e2b53d7b6be14451cfd
Jakob Buis <jakob.buis@in10.nl>
2017-07-24 17:26:45 +0200

    wip of checker class

---
+++ src/checks/BrokenLinks.rb
@@ -0,0 +1,28 @@
+require 'nokogiri'
+require_relative 'Check.rb'
+
+class BrokenLinks < Check
+
+    def check page
+        html = Nokogiri::HTML page
+        urls = html.xpath('//a[@href]').map { |a| a.attr :href }
+        urls.reject! do |url|
+            url.start_with?('tel:') || url.start_with?('mailto:')
+        end
+
+        puts urls.inspect
+        exit 13
+
+        # if page.include? 'warning'
+        #     results << {type: Check::WARNING, message: "Page contains 'warning'"}
+        # end
+
+        # ['error', 'Exception'].each do |oracle|
+        #     if page.include? oracle
+        #         results << {type: Check::ERROR, message: "Page contains an error keyword"}
+        #     end
+        # end
+
+        results
+    end
+end
