19f195dd7c94339a40606852bef9150ef1a1687b
Jakob Buis <jakob.buis@in10.nl>
2017-08-04 17:16:55 +0200

    initial implementation of broken links checker

--- src/checks/BrokenLinks.rb
+++ src/checks/BrokenLinks.rb
@@ -1,28 +1,33 @@
 require 'nokogiri'
+require 'uri'
+require 'net/http'
 require_relative 'Check.rb'
 
 class BrokenLinks < Check
 
+    def initialize scope
+        @scope = scope
+    end
+
     def check page
         html = Nokogiri::HTML page
         urls = html.xpath('//a[@href]').map { |a| a.attr :href }
-        urls.reject! do |url|
-            url.start_with?('tel:') || url.start_with?('mailto:') || url.start_with?('#')
+        results = []
+
+        # Ignore URLs that are not conventionally "followable" by a browser
+        urls.reject! { |u| u.start_with?('tel:') || u.start_with?('mailto:') || u.start_with?('#') }
+
+        # Construct to full URLs
+        urls.map! { |u| URI.join @scope, u }
+
+        urls.each do |uri|
+            http = Net::HTTP.new uri.host, uri.port
+            http.use_ssl = (uri.scheme == "https")
+            http.start do |http|
+                status = http.request(Net::HTTP::Head.new(uri)).code.to_i
+                results << {type: Check::WARNING, message: "Broken link: #{uri} (HTTP #{status})"} if status < 200 || status >= 400
+            end
         end
-
-        puts urls.inspect
-        exit 13
-
-        # if page.include? 'warning'
-        #     results << {type: Check::WARNING, message: "Page contains 'warning'"}
-        # end
-
-        # ['error', 'Exception'].each do |oracle|
-        #     if page.include? oracle
-        #         results << {type: Check::ERROR, message: "Page contains an error keyword"}
-        #     end
-        # end
-
         results
     end
 end
