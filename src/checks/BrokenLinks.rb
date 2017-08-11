14ff81ab4fcb0447d50e5b8b19043f77b02eb32b
Jakob Buis <jakob.buis@in10.nl>
2017-08-11 16:27:34 +0200

    rebuilt the logic for checking broken links

--- src/checks/BrokenLinks.rb
+++ src/checks/BrokenLinks.rb
@@ -1,5 +1,5 @@
 require 'nokogiri'
-require 'net/http'
+require 'open-uri'
 require 'addressable'
 require_relative 'Check.rb'
 
@@ -24,30 +24,50 @@ class BrokenLinks < Check
         urls.reject! { |u| u.start_with?('tel:') || u.start_with?('mailto:') || u.start_with?('#') }
 
         # Construct to full URLs
-        urls.map! { |u| Addressable::URI.join @arguments.scope, u }
+        urls.map! { |u| Addressable::URI.join(@arguments.scope, u).normalize }
 
         urls.each do |uri|
             status = status? uri
-            results << {type: Check::WARNING, message: "Broken link: #{uri} (HTTP #{status})"} if status >= 400
+            if status == 'read_timeout'
+                results << {type: Check::WARNING, message: "Broken link: #{uri} (connection timeout)"}
+            elsif status == 'socket_error'
+                results << {type: Check::WARNING, message: "Broken link: #{uri} (connection error)"}
+            elsif status == 'unknown'
+                results << {type: Check::WARNING, message: "Broken link: #{uri} (unknown error)"}
+            elsif status.to_i >= 400
+                results << {type: Check::WARNING, message: "Broken link: #{uri} (HTTP #{status})"}
+            end
         end
         results
     end
 
-    private 
+    private
 
+    # Get a cached or fresh response status
     def status? uri
-        return @cache[uri] if @cache.include? uri
+        downloadStatus(uri) unless @cache.include? uri
+        @cache[uri]
+    end
 
-        http = Net::HTTP.new uri.host, uri.port
-        http.use_ssl = (uri.scheme == "https")
-        http.start do |http|
-            request = Net::HTTP::Head.new uri
-            if @arguments.username && @arguments.password && @arguments.scope.include?(uri.host)
-                request.basic_auth @arguments.username, @arguments.password
-            end
-            status = http.request(request).code.to_i
-            @cache[uri] = status
-            return status
+    # Download the URI to get its http response status code
+    def downloadStatus uri
+        options = {read_timeout: 2}
+        # Add HTTP Basic Authentication if required
+        if @arguments.username && @arguments.password && @arguments.scope.include?(uri.host)
+            options[:http_basic_authentication] = [@arguments.username, @arguments.password]
+        end
+
+        # Get the page
+        begin
+            open(uri, options) { |response| @cache[uri] = response.status[0] }
+        rescue OpenURI::HTTPError => error
+            @cache[uri] = error.io.status[0]
+        rescue Net::ReadTimeout
+            @cache[uri] = 'read_timeout'
+        rescue SocketError
+            @cache[uri] = 'socket_error'
+        rescue
+            @cache[uri] = 'unknown'
         end
     end
 end
