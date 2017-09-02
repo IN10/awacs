bbf9937c51f8dc47a0ab682f0e8f3dab233ce823
Jakob Buis <jakob@jakobbuis.nl>
2017-09-03 01:21:22 +0200

    report img tags without src attributes separately

--- src/checks/BrokenImages.rb
+++ src/checks/BrokenImages.rb
@@ -14,11 +14,17 @@ class BrokenImages < Check
 
     def check page
         html = Nokogiri::HTML page
+        results = []
 
         urls = html.xpath('//img').map { |a| a.attr :src }
+
+        urls.reject{|u| !u.nil? }.each do |url|
+            $d.debug "testing for broken image: got <img> without src attribute"
+            results << {type: Check::WARNING, message: "Broken image: tag has no src attribute"}
+        end
+        urls.reject! {|u| u.nil?}
         urls.map! { |u| Addressable::URI.join(@arguments.scope, u).normalize }
 
-        results = []
         urls.each do |uri|
             status = @tester.status? uri
             $d.debug "testing for broken image: #{uri} -> #{status}"
