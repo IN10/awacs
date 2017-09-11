4ddff12be239e09dad07a3a80bbd2acf24db36e7
Jakob Buis <jakob@jakobbuis.nl>
2017-09-11 21:49:19 +0200

    check for dummy text like "lorem ipsum" in page

---
+++ src/checks/PrintsLorem.rb
@@ -0,0 +1,15 @@
+require_relative 'Check.rb'
+
+# Check that the page does not contain dummy content like "lorem ipsum"
+class PrintsLorem < Check
+
+    def check page
+        contents = page.downcase
+        if contents.include?('lorem') || contents.include?('ipsum')
+            $d.debug "found 'lorem' or 'ipsum' on page"
+            return [{type: Check::ERROR, message: "Page contains dummy content"}]
+        else
+            return []
+        end
+    end
+end
