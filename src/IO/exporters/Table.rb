f17ed6f81c7635873f277b03b72b37fc3b5387c6
Jakob Buis <jakob@jakobbuis.nl>
2017-10-15 19:02:24 +0200

    Formatter is now exporters/Table, filtering results a responsibility of Results instead of Table

---
+++ src/IO/exporters/Table.rb
@@ -0,0 +1,42 @@
+require 'pastel'
+require 'tty-table'
+require_relative '../../checks/Check.rb'
+
+# Format results into a pretty table
+class Table
+
+    def initialize results
+        @results = results
+    end
+
+    def render
+        data = tableData
+        return 'No pages' if data.count == 0
+        table = TTY::Table.new ['Path', 'Results'], data
+        return "Results\n" + table.render(:ascii, multiline: true)
+    end
+
+    private
+
+    def tableData
+        data = []
+        @results.all.each do |url, results|
+            data << [url, formatResults(results)]
+        end
+        data.sort! { |a,b| a[0] <=> b[0] }
+    end
+
+    def formatResults results
+        return Pastel.new.green('No problems found') if results.count == 0
+
+        results.map do |result|
+            if result[:type] == Check::WARNING
+                Pastel.new.yellow(result[:message])
+            elsif result[:type] == Check::ERROR
+                Pastel.new.red(result[:message])
+            elsif result[:type] == Check::SUCCESS
+                Pastel.new.green(result[:message])
+            end
+        end.join("\n")
+    end
+end
--- src/Results.rb
+++ src/Results.rb
@@ -16,14 +16,29 @@ class Results
     end
 
     def hasErrors?
-        entries = @pages.values.flatten(2)
+        entries = @pages.values.flatten(1)
         errors = entries.select { |entry| entry[:type] == Check::ERROR }
         errors.count > 0
     end
 
     def hasWarnings?
-        entries = @pages.values.flatten(2)
+        entries = @pages.values.flatten(1)
         warnings = entries.select { |entry| entry[:type] == Check::WARNING }
         warnings.count > 0
+    end
+
+    # Remove entries that have no errors or warnings
+    def removeSuccesses!
+        @pages.reject! { |page, entries| entries.length == 0 }
+    end
+
+    # Remove entries that have only errors
+    def removeErrors!
+        @pages.reject! { |page, entries| entries.any? { |entry| entry[:type] == Check::ERROR } }
+    end
+
+    # Remove entries that have only warnings
+    def removeWarnings!
+        @pages.reject! { |page, entries| entries.any? { |entry| entry[:type] == Check::WARNING } }
     end
 end
